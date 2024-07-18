b42b89fbf50a29b6125204875ad2ef92b4f8b15b
alissonfgc <alissonfgc01@gmail.com>
2024-07-18 09:39:03 -0300

    Implementing insert and delete on Client layer

--- api/src/main/java/com/alissonfgc/casacerta/resources/ClientResource.java
+++ api/src/main/java/com/alissonfgc/casacerta/resources/ClientResource.java
@@ -29,4 +29,23 @@ public class ClientResource {
         Client obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
+
+//    @PostMapping
+//    public ResponseEntity<User> insert(@RequestBody User obj) {
+//        obj = service.insert(obj);
+//        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+//        return ResponseEntity.created(uri).body(obj);
+//    }
+//
+//    @DeleteMapping(value = "/{id}")
+//    public ResponseEntity<Void> delete(@PathVariable Long id) {
+//        service.delete(id);
+//        return ResponseEntity.noContent().build();
+//    }
+//
+//    @PutMapping(value = "/{id}")
+//    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+//        obj = service.update(id, obj);
+//        return ResponseEntity.ok().body(obj);
+//    }
 }
--- api/src/main/java/com/alissonfgc/casacerta/services/ClientService.java
+++ api/src/main/java/com/alissonfgc/casacerta/services/ClientService.java
@@ -2,6 +2,8 @@ package com.alissonfgc.casacerta.services;
 
 import com.alissonfgc.casacerta.entities.Client;
 import com.alissonfgc.casacerta.repository.ClientRepository;
+import com.alissonfgc.casacerta.services.exceptions.DatabaseException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,4 +26,34 @@ public class ClientService {
         Optional<Client> obj = repository.findById(id);
         return obj.get();
     }
+
+    public Client insert(Client o) {
+        return repository.save(o);
+    }
+
+    public void delete(Long id) {
+        try {
+            if (findById(id) != null) {
+                repository.deleteById(id);
+            }
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+//
+//    public User update(Long id, User obj) {
+//        try {
+//            User entity = repository.getReferenceById(id);
+//            updateData(entity, obj);
+//            return repository.save(entity);
+//        } catch (EntityNotFoundException e) {
+//            throw new ResourceNotFoundException(id);
+//        }
+//    }
+//
+//    private void updateData(User entity, User obj) {
+//        entity.setName(obj.getName());
+//        entity.setEmail(obj.getEmail());
+//        entity.setPhone(obj.getPhone());
+//    }
 }

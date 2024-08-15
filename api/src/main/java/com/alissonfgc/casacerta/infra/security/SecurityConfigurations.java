0c2dbc561968931edb4027113454667853e17b74
alissonfgc <alissonfgc01@gmail.com>
2024-08-15 03:07:14 -0300

    Fixing JWT security configurations

--- api/src/main/java/com/alissonfgc/casacerta/infra/security/SecurityConfigurations.java
+++ api/src/main/java/com/alissonfgc/casacerta/infra/security/SecurityConfigurations.java
@@ -39,7 +39,7 @@ public class SecurityConfigurations {
                         .anyRequest().authenticated()
                 )
                 .build();
-    }
+    }git
 
     @Bean
     WebSecurityCustomizer ignoringCustomizer() {
